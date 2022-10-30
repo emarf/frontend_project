9389b37d745d6e4e4887c6488e8671b1db606f13
Roman Ivashekvich <rivashkevich@solvd.com>
2022-10-31 02:30:44 +0300

    created first shared and widget components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,9 @@
-import { Link } from 'react-router-dom';
 import './styles/index.scss';
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from './providers/ThemeProvider';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
+
 
 
 
@@ -11,9 +12,8 @@ const App = () => {
 
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
       <AppRouter />
     </div>
   );
