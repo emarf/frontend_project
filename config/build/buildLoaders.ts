054388adee3704307063f574ffbd478d5ad00737
Roman Ivashekvich <rivashkevich@solvd.com>
2022-11-06 16:57:52 +0300

    added babel, extract plagin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,23 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const babelLoader = {
+      test: /\.(js|jsx|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            ["i18next-extract", {
+              locales: ['ru','en'],
+              keyAsDefaultValue: true,
+            }],
+          ]
+        }
+      }
+    }
+
     const svgLoader = {
       test: /\.svg$/,
       use: ['@svgr/webpack'],
@@ -47,6 +64,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ]
